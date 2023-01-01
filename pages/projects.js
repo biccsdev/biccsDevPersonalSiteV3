a48afdeeb17832100adbc1774e3f61dd88b6b4e9
vicbears <victor.garcia@geekbears.com>
2022-12-31 20:26:12 -0700

    Working on the projects tab, first component done. Last commit of the year... Happy new yeargit add .

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Projects.module.css'
 
 export default function Project() {
     return (
@@ -26,6 +26,36 @@ export default function Project() {
                     content="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
                 />
             </Head>
+            <main className={styles.mainContainer}>
+                <div className={styles.cardsContainer}>
+                    <div className={styles.card}>
+                        <h2>Token gated system (SMB frames)</h2>
+                        <Image src={'/projects/videoSMB.gif'} alt='token gated app gif example' width={300} height={300} />
+                        <p> SMB Frames is an app I made as a proof of concept about token gated content for a Solana NFT collection, made with NextJS for the front-end and Metaplex, Anchor and @solana/web3.js for the token gated system implementation.
+                            This is done to incentivize users to take part in a platform, or to provide exclusive access to certain content or services.
+                            Token gating can be used to create loyalty programs, reward content creators, or to drive user engagement.</p>
+                        <a href='https://smbframes.vercel.app/' target='_blank'>
+                            <button className={styles.cardButton}>Go to site</button>
+                        </a>
+                        <a href='https://github.com/biccsdev/vercelSMBFrames/tree/main' target='_blank'>
+                            <button className={styles.cardButton}>See code</button>
+                        </a>
+                    </div>
+                    <div className={styles.card}>
+                        <h2>Token gated system (SMB frames)</h2>
+                        <Image src={'/projects/videoSMB.gif'} alt='token gated app gif example' width={300} height={300} />
+                        <p> SMB Frames is an app I made as a proof of concept about token gated content for a Solana NFT collection, made with NextJS for the front-end and Metaplex, Anchor and @solana/web3.js for the token gated system implementation.
+                            This is done to incentivize users to take part in a platform, or to provide exclusive access to certain content or services.
+                            Token gating can be used to create loyalty programs, reward content creators, or to drive user engagement.</p>
+                        <a href='https://smbframes.vercel.app/' target='_blank'>
+                            <button className={styles.cardButton}>Go to site</button>
+                        </a>
+                        <a href='https://github.com/biccsdev/vercelSMBFrames/tree/main' target='_blank'>
+                            <button className={styles.cardButton}>See code</button>
+                        </a>
+                    </div>
+                </div>
+            </main>
         </Layout>
     )
 }
