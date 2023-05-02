c170e325cf7ed0753c3dcc12613e619f95c6a125
biccsdev <biccsdev@gmail.com>
2023-05-01 19:41:15 -0600

    fished bug where mobile version would look blank, removed from layoutu.module.css .headerContainer class property min-height

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link';
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
-import pfp from '../public/pfp.png'
 
-import dynamic from "next/dynamic";
-const Animator = dynamic(
-  import("react-scroll-motion").then((it) => it.Animator),
-  { ssr: false }
-);
 
-import { ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
-
-import { useEffect } from 'react';
 
 Home.siteTitle = "";
 Home.description = "hi";
@@ -41,7 +31,6 @@ export default function Home() {
           property="og:image"
           content="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
         />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" />
       </Head>
       <div className={styles.main}>
         <div className={styles.title}>
