cee4e1425da2e401d739716f4a4004b60d59c272
biccsdev <biccsdev@gmail.com>
2023-10-15 17:56:10 -0600

    changes to the navbar

--- components/layout.js
+++ components/layout.js
@@ -3,183 +3,64 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
 
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 import homeFavicon from "../public/home.ico";
 
 export default function Layout({ children, home, about, projects, blogs, archives }) {
+    const router = useRouter();
+    const [selectedLink, setSelectedLink] = useState('');
+
+    const navLinks = [
+        { path: '/', text: 'HOME' },
+        { path: '/about', text: 'ABOUT ME' },
+        { path: '/projects', text: 'PROJECTS' },
+        { path: '/blogs', text: 'BLOGS' },
+        { path: '/archives', text: 'ARCHIVES' },
+    ];
+
+    useEffect(() => {
+        setSelectedLink(router.pathname);
+    }, [router.pathname]);
+
     return (
         <div className={styles.main}>
             <Head>
                 <link rel="icon" href={`${homeFavicon}`} />
                 <meta
                     name="description"
-                    content="biccsdev personal portafolio"
+                    content="biccs personal site"
                 />
                 <meta
                     property="og:image"
                     content={`https://og-image.vercel.app/${encodeURI(
-                        "biccs dev's personal site",
+                        "biccs personal site",
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
                 <meta name="og:title" content="home" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <title>Home</title>
             </Head>
-            {(home) &&
-                <header className={styles.headerContainer}>
-                    <input className={styles.checkbox} type="checkbox" name="" id="" />
-                    <div className={styles.hamburgerLines}>
-                        <span className={`${styles.line} ${styles.line1}`}></span>
-                        <span className={`${styles.line} ${styles.line2}`}></span>
-                        <span className={`${styles.line} ${styles.line3}`}></span>
+            <header className={styles.headerContainer}>
+                <input className={styles.checkbox} type="checkbox" name="" id="" />
+                <div className={styles.hamburgerLines}>
+                    <span className={`${styles.line} ${styles.line1}`}></span>
+                    <span className={`${styles.line} ${styles.line2}`}></span>
+                    <span className={`${styles.line} ${styles.line3}`}></span>
+                </div>
+                <div className={styles.menuItems}>
+                    <div className={styles.items}>
+                        {navLinks.map((link) => (
+                            <Link key={link.path} href={link.path}>
+                                <h1 className={selectedLink === link.path ? styles.selected : ''}>
+                                    {link.text}
+                                </h1>
+                            </Link>
+                        ))}
                     </div>
-                    <div className={styles.menuItems}>
-                        <h1 style={{ fontSize: '40px' }}>biccs<span style={{ color: '#7ae582' }}>dev</span></h1>
-                        <div className={styles.items}>
-                            <Link href='/'>
-                                <h1 className={styles.selected}>HOME</h1>
-                            </Link>
-                            <Link href='/about'>
-                                <h1>ABOUT ME</h1>
-                            </Link>
-                            <Link href='/projects'>
-                                <h1>PROJECTS</h1>
-                            </Link>
-                            <Link href='/blogs'>
-                                <h1>BLOGS</h1>
-                            </Link>
-                            <Link href='/archives'>
-                                <h1>ARCHIVES</h1>
-                            </Link>
-                        </div>
-                    </div>
-                </header>
-            }
-            {
-                (about) &&
-                <header className={styles.headerContainer}>
-                    <input className={styles.checkbox} type="checkbox" name="" id="" />
-                    <div className={styles.hamburgerLines}>
-                        <span className={`${styles.line} ${styles.line1}`}></span>
-                        <span className={`${styles.line} ${styles.line2}`}></span>
-                        <span className={`${styles.line} ${styles.line3}`}></span>
-                    </div>
-                    <div className={styles.menuItems}>
-                        <h1 style={{ fontSize: '40px' }}>biccs<span style={{ color: '#7ae582' }}>dev</span></h1>
-                        <div className={styles.items}>
-                            <Link href='/'>
-                                <h1 >HOME</h1>
-                            </Link>
-                            <Link href='/about'>
-                                <h1 className={styles.selected}>ABOUT ME</h1>
-                            </Link>
-                            <Link href='/projects'>
-                                <h1>PROJECTS</h1>
-                            </Link>
-                            <Link href='/blogs'>
-                                <h1>BLOGS</h1>
-                            </Link>
-                            <Link href='/archives'>
-                                <h1>ARCHIVES</h1>
-                            </Link>
-                        </div>
-                    </div>
-                </header>
-            }
-            {
-                (projects) &&
-                <header className={styles.headerContainer}>
-                    <input className={styles.checkbox} type="checkbox" name="" id="" />
-                    <div className={styles.hamburgerLines}>
-                        <span className={`${styles.line} ${styles.line1}`}></span>
-                        <span className={`${styles.line} ${styles.line2}`}></span>
-                        <span className={`${styles.line} ${styles.line3}`}></span>
-                    </div>
-                    <div className={styles.menuItems}>
-                        <h1 style={{ fontSize: '40px' }}>biccs<span style={{ color: '#7ae582' }}>dev</span></h1>
-                        <div className={styles.items}>
-                            <Link href='/'>
-                                <h1 >HOME</h1>
-                            </Link>
-                            <Link href='/about'>
-                                <h1>ABOUT ME</h1>
-                            </Link>
-                            <Link href='/projects'>
-                                <h1 className={styles.selected}>PROJECTS</h1>
-                            </Link>
-                            <Link href='/blogs'>
-                                <h1>BLOGS</h1>
-                            </Link>
-                            <Link href='/archives'>
-                                <h1>ARCHIVES</h1>
-                            </Link>
-                        </div>
-
-                    </div>
-                </header>
-            }
-            {
-                (blogs) &&
-                <header className={styles.headerContainer}>
-                    <input className={styles.checkbox} type="checkbox" name="" id="" />
-                    <div className={styles.hamburgerLines}>
-                        <span className={`${styles.line} ${styles.line1}`}></span>
-                        <span className={`${styles.line} ${styles.line2}`}></span>
-                        <span className={`${styles.line} ${styles.line3}`}></span>
-                    </div>
-                    <div className={styles.menuItems}>
-                        <h1 style={{ fontSize: '40px' }}>biccs<span style={{ color: '#7ae582' }}>dev</span></h1>
-                        <div className={styles.items}>
-                            <Link href='/'>
-                                <h1 >HOME</h1>
-                            </Link>
-                            <Link href='/about'>
-                                <h1>ABOUT ME</h1>
-                            </Link>
-                            <Link href='/projects'>
-                                <h1>PROJECTS</h1>
-                            </Link>
-                            <Link href='/blogs'>
-                                <h1 className={styles.selected}>BLOGS</h1>
-                            </Link>
-                            <Link href='/archives'>
-                                <h1>ARCHIVES</h1>
-                            </Link>
-                        </div>
-
-                    </div>
-                </header>
-            }
-            {
-                (archives) &&
-                <header className={styles.headerContainer}>
-                    <input className={styles.checkbox} type="checkbox" name="" id="" />
-                    <div className={styles.hamburgerLines}>
-                        <span className={`${styles.line} ${styles.line1}`}></span>
-                        <span className={`${styles.line} ${styles.line2}`}></span>
-                        <span className={`${styles.line} ${styles.line3}`}></span>
-                    </div>
-                    <div className={styles.menuItems}>
-                        <h1 style={{ fontSize: '40px' }}>biccs<span style={{ color: '#7ae582' }}>dev</span></h1>
-                        <div className={styles.items}>
-                            <Link href='/'>
-                                <h1 >HOME</h1>
-                            </Link>
-                            <Link href='/about'>
-                                <h1>ABOUT ME</h1>
-                            </Link>
-                            <Link href='/projects'>
-                                <h1>PROJECTS</h1>
-                            </Link>
-                            <Link href='/blogs'>
-                                <h1>BLOGS</h1>
-                            </Link>
-                            <Link href='/archives'>
-                                <h1 className={styles.selected}>ARCHIVES</h1>
-                            </Link>
-                        </div>
-                    </div>
-                </header>
-            }
+                </div>
+            </header>
             <main className={styles.mainContainer}>{children}</main>
         </div >
     );
