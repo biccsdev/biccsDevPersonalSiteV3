279d81a8b379d28335727ba3cbc86db949fb2640
biccsdev <biccsdev@gmail.com>
2023-10-18 15:28:19 -0600

    refactored projects tab UX

--- pages/projects.js
+++ pages/projects.js
@@ -8,11 +8,20 @@ import React, { useState, useEffect } from 'react';
 export default function Project() {
     const [projectsData, setProjectsData] = useState([]);
     const [showFullDescriptions, setShowFullDescriptions] = useState(Array(projectsData.length).fill(false));
+    const [selectedCard, setSelectedCard] = useState(null);
 
     const toggleDescription = (index) => {
         const newShowFullDescriptions = [...showFullDescriptions];
         newShowFullDescriptions[index] = !showFullDescriptions[index];
         setShowFullDescriptions(newShowFullDescriptions);
+    };
+
+    const openCard = (id) => {
+        setSelectedCard(id);
+    };
+
+    const closeCard = () => {
+        setSelectedCard(null);
     };
 
     useEffect(() => {
@@ -57,11 +66,10 @@ export default function Project() {
             <main className={styles.mainContainer}>
                 <div className={styles.cardsContainer}>
                     {projectsData.map(item => (
-                        <div className={styles.card}>
+                        <div className={styles.card} onClick={() => openCard(item.id)} key={item.id}>
                             {/* <Image src={'/projects/videoSMB.gif'} alt='token gated app gif example' width={300} height={300} /> */}
                             <div className={styles.cardInfo}>
                                 <h2>{item.title}</h2>
-                                {/* <p>{item.description}</p> */}
                                 <p>
                                     {showFullDescriptions[item.id]
                                         ? item.description // Show full description when clicked
@@ -80,82 +88,30 @@ export default function Project() {
                             </div>
                         </div>
                     ))}
-                    {/* <div className={styles.card}>
-                        <Image src={'/projects/videoSMB.gif'} alt='token gated app gif example' width={300} height={300} />
-                        <div className={styles.cardInfo}>
-                            <h2>Blockchain Token Gated System</h2>
-                            <p> This is an app I made as a proof of concept about token gated content as a use case for web3, made with NextJS for the front-end and Metaplex, Anchor and @solana/web3.js for the token gated system implementation.
-                                This is done to incentivize users to take part in a platform, or to provide exclusive access to certain content or services.
-                                Token gating can be used to create loyalty programs, reward content creators, or to drive user engagement.</p>
-                            <a href='https://smbframes.vercel.app/' target='_blank' rel="noreferrer">
-                                <button className={styles.cardButton}>Go to site</button>
-                            </a>
-                            <a href='https://github.com/biccsdev/vercelSMBFrames/tree/main' target='_blank' rel="noreferrer">
-                                <button className={styles.cardButton}>See code</button>
-                            </a>
-                        </div>
-                    </div>
-                    <div className={styles.card}>
-                        <Image src={'/twtlogo.png'} alt='token gated app gif example' width={300} height={300} />
-                        <div className={styles.cardInfo}>
-                            <h2>Twitter Automated Blockchain Mint Bot</h2>
-                            <p> This twitter bot was made as a contribution to a web3 community, in order to increase the noise about the collection and make more people aware of this community.
-                                It was made using twitter API V2 for interacting with twitter, ExpressJS for the backend and @solana/web3.js library for interacting with the Solana Blockchain.
-                                Twitter bots can be a great way to keep your followers up to date on the latest mints of your Digital Collectibles collection.
-                                By using a Twitter bot, you can quickly and easily share the new mints with your followers,
-                                helping to create excitement and interest in your collection. This can help to increase engagement and sales,
-                                as well as give your collection more visibility and recognition. In addition, it can also be a great way to
-                                let your followers know when something new and exciting is coming out, giving them a reason to come back
-                                and check out your collection.</p>
-                            <a href='https://twitter.com/villinzbot' target='_blank' rel="noreferrer">
-                                <button className={styles.cardButton}>Go to site</button>
-                            </a>
-                            <a href='https://github.com/biccsdev/villinzBotV2' target='_blank' rel="noreferrer">
-                                <button className={styles.cardButton}>See code</button>
-                            </a>
-                        </div>
 
-                    </div>
-                    <div className={styles.card}>
-                        <Image src={'/projects/shortUrl.jpg'} alt='token gated app gif example' width={300} height={300} />
-                        <div className={styles.cardInfo}>
-                            <h2>Backend URL Shortener API</h2>
-                            <p>Made with NestJS and Typescript for the backend and Mongoose for the Database.
-                                This System  was developed using the Module, Controller, Service, architecture
-                                where the Module was needed to allow the Mongoose and UrlShortener modules functionality to be used within the main module.
-                                The controller layer was used to intercept the API calls and call the Service layer in order to execute the business logic of the system (shorten the url).
-                            </p>
-                            <a href='https://github.com/biccsdev/urlshortener' target='_blank' rel="noreferrer">
-                                <button className={styles.cardButton}>See code</button>
-                            </a>
+                    {selectedCard !== null && (
+                        <div className={styles.overlay}>
+                            <div className={styles.cardPopUp}>
+                                <h2>{projectsData[selectedCard].title}</h2>
+                                <Image src={projectsData[selectedCard].media} alt='media related to project' height={300} width={300}></Image>
+                                <p>{projectsData[selectedCard].description}</p>
+                                {(selectedCard.codeLink != "") && (
+                                    <a href={selectedCard.codeLink} target='_blank' rel="noreferrer">
+                                        <button className={styles.cardButton}>See code</button>
+                                    </a>
+                                )}
+                                {(selectedCard.siteLink != "") && (
+                                    <a href={selectedCard.siteLink} target='_blank' rel="noreferrer">
+                                        <button className={styles.cardButton}>Go to site</button>
+                                    </a>
+                                )}
+                                {/* Add more details here */}
+                                <button className={styles.cardButton} onClick={closeCard}>
+                                    Close
+                                </button>
+                            </div>
                         </div>
-
-                    </div>
-                    <div className={styles.card}>
-                        <Image src={'/projects/chatAppLogo.png'} alt='token gated app gif example' width={300} height={300} />
-                        <div className={styles.cardInfo}>
-                            <h2>Full Stack Chat App</h2>
-                            <p>This chat app was made with NodeJs implementing ExpressJS and Mongoose for the backend, and Html, css and JS vanilla for the Frontend.
-                                This app makes use of websocket for bi-directional, full-duplex communication between the client and server. Websockets allow for real-time
-                                communication between the two endpoints, allowing for faster and more efficient communication.  </p>
-                            <a href='https://github.com/biccsdev/chatApp' target='_blank' rel="noreferrer">
-                                <button className={styles.cardButton}>See code</button>
-                            </a>
-                        </div>
-
-                    </div>
-                    <div className={styles.card}>
-                        <Image src={'/projects/vlnzgif.gif'} alt='token gated app gif example' width={300} height={300} />
-                        <div className={styles.cardInfo}>
-                            <h2>Token Gated Solana Wallets Explorer</h2>
-                            <p>This system was build using NextJS and Typescript with @Solana/web3.js library to be able to
-                                connect to the Blockchain. It allows holders of certain token to access the wallet finder
-                                system, where they input a Wallet Address and retrieve those wallet's assets.</p>
-                            <a href='https://villinz-hub.vercel.app/' target='_blank' rel="noreferrer">
-                                <button className={styles.cardButton}>Visit Site</button>
-                            </a>
-                        </div>
-                    </div> */}
+                    )}
                 </div>
             </main>
         </Layout>
